698bcb644f33cc3f242927522a9fc1169394b10c
nupursharma24 <84871394+nupursharma24@users.noreply.github.com>
2023-08-26 12:36:05 +0530

    Create LargestNumber.py
    New file TDS project

---
+++ LargestNumber.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.write("Highest Number")
+st.header("Input Numbers")
+def user_input():
+  n1 = st.number_input("1st Number")
+  n2 = st.number_input("2nd Number")
+  n3 = st.number_input("3rd Number")
+  return n1,n2,n3
+a,b,c = user_input()
+if a >= b and a >= c:
+  st.write(a)
+elif a >= b and a <= c:
+  st.write(c)
+else:
+  st.write(b)
